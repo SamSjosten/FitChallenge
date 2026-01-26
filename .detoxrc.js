8f000efb73d779682af8d277183e2c1ecba8e88b
Samuel.Sjosten <samsjosten1911@gmail.com>
2026-01-26 15:31:18 -0600

    feat: add Detox configuration for iOS and Android E2E testing

---
+++ .detoxrc.js
@@ -0,0 +1,47 @@
+/** @type {Detox.DetoxConfig} */
+module.exports = {
+  testRunner: {
+    args: {
+      $0: "jest",
+      config: "e2e/jest.config.js",
+    },
+    jest: {
+      setupTimeout: 120000,
+    },
+  },
+  apps: {
+    "ios.release": {
+      type: "ios.app",
+      binaryPath:
+        "ios/build/Build/Products/Release-iphonesimulator/FitChallenge.app",
+      build:
+        "xcodebuild -workspace ios/FitChallenge.xcworkspace -scheme FitChallenge -configuration Release -sdk iphonesimulator -derivedDataPath ios/build",
+    },
+    "android.release": {
+      type: "android.apk",
+      binaryPath: "android/app/build/outputs/apk/release/app-release.apk",
+      build:
+        "cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release",
+    },
+  },
+  devices: {
+    simulator: {
+      type: "ios.simulator",
+      device: { type: "iPhone 15" },
+    },
+    emulator: {
+      type: "android.emulator",
+      device: { avdName: "Pixel_4_API_34" },
+    },
+  },
+  configurations: {
+    "ios.sim.release": {
+      device: "simulator",
+      app: "ios.release",
+    },
+    "android.emu.release": {
+      device: "emulator",
+      app: "android.release",
+    },
+  },
+};
