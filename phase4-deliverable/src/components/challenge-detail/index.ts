4a31bc68017548e614e01b7b013ded0478de1356
Samuel.Sjosten <samsjosten1911@gmail.com>
2026-02-07 11:18:40 -0600

    Deleted Phase 4 zip

--- phase4-deliverable/src/components/challenge-detail/index.ts
+++
@@ -1,5 +0,0 @@
-// src/components/challenge-detail/index.ts
-// Exports for Challenge Detail Screen
-
-export { ChallengeDetailScreen } from "./ChallengeDetailScreen";
-export type { ChallengeDetailScreenProps } from "./ChallengeDetailScreen";
--- phase4-deliverable/src/components/notifications/index.ts
+++
@@ -1,6 +0,0 @@
-// src/components/notifications/index.ts
-// Notification components
-
-export { NotificationsScreen } from "./NotificationsScreen";
-export { NotificationRow } from "./NotificationRow";
-export { NotificationFilters } from "./NotificationFilters";
